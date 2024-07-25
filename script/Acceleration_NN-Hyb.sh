89b08ebdcc0a877386d26e07d60a502288068403
lzzh97 <118946483+lzzh97@users.noreply.github.com>
2024-07-25 12:11:22 -0500

    Add files via upload

---
+++ script/Acceleration_NN-Hyb.sh
@@ -0,0 +1,22 @@
+﻿sed -i 's/\r//g' script/all-fault-cases.sh
+echo "NN-Hyb for all faults case with/without Lookup table"
+echo -e "\n"
+
+cd ./NN\_Hyb
+echo "K=10000"
+echo "without Lookup table"
+./atalanta -b 10000 -w ../circuits/c1908.bench
+./atalanta -b 10000 -w ../circuits/c2670.bench
+./atalanta -b 10000 -w ../circuits/c3540.bench
+./atalanta -b 10000 -w ../circuits/c5315.bench
+./atalanta -b 10000 -w ../circuits/c6288.bench
+./atalanta -b 10000 -w ../circuits/c7552.bench
+
+echo "with Lookup table"
+./atalanta -b 10000 -w ../circuits/c1908.bench
+./atalanta -b 10000 -w ../circuits/c2670.bench
+./atalanta -b 10000 -w ../circuits/c3540.bench
+./atalanta -b 10000 -w ../circuits/c5315.bench
+./atalanta -b 10000 -w ../circuits/c6288.bench
+./atalanta -b 10000 -w ../circuits/c7552.bench
+cd ..
