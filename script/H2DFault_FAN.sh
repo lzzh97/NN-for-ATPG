4647e6164dab79bce714223fdf2e75e030617813
lzzh97 <118946483+lzzh97@users.noreply.github.com>
2024-07-25 10:16:51 -0500

    Update H2DFault_FAN.sh

--- script/H2DFault_FAN.sh
+++ script/H2DFault_FAN.sh
@@ -2,7 +2,7 @@
 echo "FAN for hard-to-detect faults case"
 echo -e "\n"
 
-cd ./FAN
+cd ./FAN # cd to Atalanta source code from https://github.com/hsluoyz/Atalanta
 echo "K=25000"
 ./atalanta -b 25000 -f ../hard_faults/c2670_h2d.ufaults ../circuits/c2670.bench
 ./atalanta -b 25000 -f ../hard_faults/c6288_h2d.ufaults ../circuits/c6288.bench
