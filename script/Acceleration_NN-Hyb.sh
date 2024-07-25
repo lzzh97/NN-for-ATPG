5f700bb44e96737f751f7e6c8f516d7f39073c82
lzzh97 <118946483+lzzh97@users.noreply.github.com>
2024-07-25 12:13:24 -0500

    Update Acceleration_NN-Hyb.sh

--- script/Acceleration_NN-Hyb.sh
+++ script/Acceleration_NN-Hyb.sh
@@ -13,10 +13,10 @@ echo "without Lookup table"
 ./atalanta -b 10000 -w ../circuits/c7552.bench
 
 echo "with Lookup table"
-./atalanta -b 10000 -w ../circuits/c1908.bench
-./atalanta -b 10000 -w ../circuits/c2670.bench
-./atalanta -b 10000 -w ../circuits/c3540.bench
-./atalanta -b 10000 -w ../circuits/c5315.bench
-./atalanta -b 10000 -w ../circuits/c6288.bench
-./atalanta -b 10000 -w ../circuits/c7552.bench
+./atalanta -b 10000 ../circuits/c1908.bench
+./atalanta -b 10000 ../circuits/c2670.bench
+./atalanta -b 10000 ../circuits/c3540.bench
+./atalanta -b 10000 ../circuits/c5315.bench
+./atalanta -b 10000 ../circuits/c6288.bench
+./atalanta -b 10000 ../circuits/c7552.bench
 cd ..
